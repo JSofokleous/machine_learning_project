e0f08f8ec4e9ed41acdeb811d40c0b236023a188
Jack Sofokleous <jack.sofokleous8@gmail.com>
2023-01-23 14:12:13 +0000

    Updated bee_pop.py
    Added linear regression model which predicts honey production up to 2050

--- bee_pop.py
+++ bee_pop.py
@@ -32,15 +32,24 @@ plt.ylabel("Total Production")
 
 ## FIT A LINEAR REGRESSION MODEL
 
+# Fit linear regression model to get coefficients
 model = linear_model.LinearRegression()
 fit = model.fit(X, y)
 y_predict = fit.predict(X)
+
+# Plot model. Set x-axis range to 2050 to account for future modelling
 plt.plot(X, y_predict, c='r')
+plt.xlim(1995, 2050)
+
+
+## PREDICT FUTURE HONEY PRODUCTION
+
+# Create array of future dates from 2013 to 2050 and reshape to a 2D array
+X_future = np.array(range(2011, 2051))
+X_future = X_future.reshape(-1, 1)
+
+# Model future honey production
+y_future_predict = fit.predict(X_future)
+plt.plot(X_future, y_future_predict, c='r')
+
 plt.show()
-
-## PREDICT
-
-print(fit.predict(2050))
-#
-
-
